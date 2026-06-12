1c0c4ab49ca13dec4ad7e7c55b54cece9a433fcf
fake-ppsan <itzppsan@gmail.com>
2026-06-12 21:24:47 +0530

    Move constants to constants.py

---
+++ constants.py
@@ -0,0 +1,25 @@
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 740
+
+SQUARE_SIZE = 64
+
+BOARD_SIZE = 8 * SQUARE_SIZE
+
+OFFSET_X = (SCREEN_WIDTH - BOARD_SIZE) // 2
+OFFSET_Y = (SCREEN_HEIGHT - BOARD_SIZE) // 2
+
+CAPTURE_SORT_ORDER = {
+    "p": 1,
+    "n": 2,
+    "b": 3,
+    "r": 4,
+    "q": 5
+    }
+
+PIECE_VALUE = {
+    "p": 1,
+    "n": 3,
+    "b": 3,
+    "r": 5,
+    "q": 9
+    }
